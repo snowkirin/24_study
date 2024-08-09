16e544c15c92c4977b7fecb2d640e050053701fc
jinsoo <JinSoo.Jung@hyper-tech.co.kr>
2024-08-09 17:07:23 +0900

    router-setting contacts 이상으로 인한 작업 중지

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/root';
+import Contact from './routes/contact';
 import ErrorPage from './error-page';
 import './index.css'
 
@@ -9,8 +10,16 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement:  <ErrorPage />
-  }
+    // 해당 경로에 대한 에러 페이지를 설정합니다.
+    errorElement:  <ErrorPage />,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />
+      }
+    ]
+  },
+  
 ])
 
 createRoot(document.getElementById('root')!).render(
